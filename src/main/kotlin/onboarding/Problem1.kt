843515cf517a891f941a6c1c7322e1c74f7f2661
박지민 <pjm5489@naver.com>
2024-09-15 03:30:14 +0900

    refactor: makePageNumbers() 삭제
    불필요

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,24 +1,33 @@
 package onboarding
 
 import camp.nextstep.edu.missionutils.Randoms
+import kotlin.math.max
 
-//fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-//
-//}
+fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+    val pobiScore = makeScore(pobi)
+    val crongScore = makeScore(crong)
 
-fun makePageNumbers(): List<Int> {
-    var leftPage = 0
-    var rightPage = 0
-    val pageNumbers = mutableListOf<Int>()
-    while (leftPage % 2 == 0) {
-        leftPage = Randoms.pickNumberInRange(3, 397)
+
+    if (pobiScore > crongScore) {
+        return 1
     }
-    rightPage = leftPage + 1
-    pageNumbers.add(0,leftPage)
-    pageNumbers.add(1,rightPage)
-    return pageNumbers
 }
 
-fun main() {
-    makePageNumbers()
+fun makeScore(who: List<Int>): Int {
+    var leftPageSum = 0
+    var rightPageSum = 0
+    var leftPageProduct = 1
+    var rightPageProduct = 1
+    for (i in 0 until who[0].toString().length) {
+        leftPageSum += who[0].toString().split("")[i].toInt()
+        rightPageSum += who[1].toString().split("")[i].toInt()
+
+        leftPageProduct *= who[0].toString().split("")[i].toInt()
+        rightPageProduct *= who[1].toString().split("")[i].toInt()
+    }
+    val leftPageMax = max(leftPageSum, leftPageProduct)
+    val rightPageMax = max(rightPageSum, rightPageProduct)
+    val totalMax = max(leftPageMax, rightPageMax)
+
+    return totalMax
 }
