ca90b749d5f6093bebececaea62a7fc0289467e5
박지민 <pjm5489@naver.com>
2024-09-15 02:23:22 +0900

    feat: makePageNumbers() 구현
    무작위 두페이지를 만드는 메서드 구현

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,24 @@
 package onboarding
 
-fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+import camp.nextstep.edu.missionutils.Randoms
+
+//fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+//
+//}
+
+fun makePageNumbers(): List<Int> {
+    var leftPage = 0
+    var rightPage = 0
+    val pageNumbers = mutableListOf<Int>()
+    while (leftPage % 2 == 0) {
+        leftPage = Randoms.pickNumberInRange(3, 397)
+    }
+    rightPage = leftPage + 1
+    pageNumbers.add(0,leftPage)
+    pageNumbers.add(1,rightPage)
+    return pageNumbers
+}
+
+fun main() {
+    makePageNumbers()
 }
