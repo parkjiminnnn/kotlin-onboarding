47243154b82be63f69fef30ca9dfdfa95bae3a79
박지민 <pjm5489@naver.com>
2024-09-15 04:44:05 +0900

    refactor: solution1메서드 로직 변경

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,16 +1,22 @@
 package onboarding
 
-import camp.nextstep.edu.missionutils.Randoms
 import kotlin.math.max
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
     val pobiScore = makeScore(pobi)
     val crongScore = makeScore(crong)
 
+    if (pobi[0] + 1 == pobi[1] && crong[0] + 1 == crong[1]) {
+        if (pobiScore > crongScore) {
+            return 1
+        } else if (pobiScore < crongScore) {
+            return 2
+        } else if (pobiScore == crongScore) {
+            return 0
+        }
 
-    if (pobiScore > crongScore) {
-        return 1
     }
+    return -1
 }
 
 fun makeScore(who: List<Int>): Int {
@@ -18,12 +24,13 @@ fun makeScore(who: List<Int>): Int {
     var rightPageSum = 0
     var leftPageProduct = 1
     var rightPageProduct = 1
-    for (i in 0 until who[0].toString().length) {
-        leftPageSum += who[0].toString().split("")[i].toInt()
-        rightPageSum += who[1].toString().split("")[i].toInt()
-
-        leftPageProduct *= who[0].toString().split("")[i].toInt()
-        rightPageProduct *= who[1].toString().split("")[i].toInt()
+    for (char in who[0].toString()) {
+        leftPageSum += char.toString().toInt()
+        leftPageProduct *= char.toString().toInt()
+    }
+    for (char in who[1].toString()) {
+        rightPageSum += char.toString().toInt()
+        rightPageProduct *= char.toString().toInt()
     }
     val leftPageMax = max(leftPageSum, leftPageProduct)
     val rightPageMax = max(rightPageSum, rightPageProduct)
