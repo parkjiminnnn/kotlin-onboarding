27debbe8d372715fff0a690d19f10831d527b2d6
박지민 <pjm5489@naver.com>
2024-09-16 18:10:45 +0900

    feat: 3,6,9횟수를 세는 로직 구현

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -5,6 +5,17 @@ fun solution3(number: Int): Int {
     val numberArray = mutableListOf<String>()
     for (i in 0 until number) {
         numberArray.add(i, "${i + 1}")
+        val numberItemList = numberArray[i].split("")
+        for (element in numberItemList) {
+            if (element.contains("3")) {
+                clapTimes++
+            } else if (element.contains("6")) {
+                clapTimes++
+            } else if (element.contains("9")) {
+                clapTimes++
+            }
+        }
+
     }
     return clapTimes
 }
