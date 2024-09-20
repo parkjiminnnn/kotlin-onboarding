10c747aa14f66191ab2fc13aea6215753a105d21
박지민 <pjm5489@naver.com>
2024-09-20 20:36:01 +0900

    feat: 정렬을 제외한 나머지 기능들 구현

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,31 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    val sharedFriends = mutableListOf<List<Any>>()
+    val sharedFriendsCount = mutableListOf<List<Any>>()
+    var visitorCount = 0
+    for (i in friends.indices) {
+        for (j in friends[i].indices) {
+            when (user) {
+                friends[i][0] -> {
+                    sharedFriends.add(listOf(friends[i][1], 10))
+                }
+
+                friends[i][1] -> sharedFriends.add(listOf(friends[i][0], 10))
+            }
+        }
+    }
+    //sharedfriends = {[a,10][b,10][a,10][c,10]}
+    for (k in sharedFriends.indices) {
+        sharedFriendsCount.add(listOf(sharedFriends[k][0], sharedFriends[k].count() * 10))
+        for (l in visitors.indices) {
+            visitorCount = visitors[l].count()
+            if (sharedFriends[k][0] == visitors[l]) {
+                sharedFriendsCount[k] = listOf(sharedFriends[k][0],sharedFriends[k].count() * 10 + visitorCount)
+            }
+        }
+    }
+    
+
+
 }
