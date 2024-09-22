8424fd59377d1403060acaa50491ab4e69172258
박지민 <pjm5489@naver.com>
2024-09-22 14:51:24 +0900

    refactor: sharedFriendsScore 로직변경

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,40 +5,58 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    val sharedFriends = mutableListOf<List<Any>>()
+    val sharedFriendList = mutableListOf<String>()
+    val userFriendList = mutableSetOf<String>()
     val sharedFriendsScore = mutableListOf<List<Any>>()
     val result = mutableListOf<String>()
 
     for (i in friends.indices) {
         for (j in friends.indices) {
             if (user == friends[i][0]) {
+                userFriendList.add(friends[i][1])
                 when {
                     friends[i][1] == friends[j][1] && i != j -> {
-                        sharedFriends.add(listOf(friends[j][0], 10))
+                        sharedFriendList.add(friends[j][0])
                     }
 
                     friends[i][1] == friends[j][0] -> {
-                        sharedFriends.add(listOf(friends[j][1], 10))
+                        sharedFriendList.add(friends[j][1])
                     }
                 }
             } else if (user == friends[i][1]) {
+                userFriendList.add(friends[i][0])
                 when {
                     friends[i][0] == friends[j][0] && i != j -> {
-                        sharedFriends.add(listOf(friends[j][1], 10))
+                        sharedFriendList.add(friends[j][1])
                     }
 
                     friends[i][0] == friends[j][1] -> {
-                        sharedFriends.add(listOf(friends[j][0], 10))
+                        sharedFriendList.add(friends[j][0])
                     }
                 }
             }
         }
     }
-    for (k in sharedFriends.indices) {
-        if (!sharedFriendsScore.toString().contains(sharedFriends[k][0].toString())) {
-            sharedFriendsScore.add(listOf(sharedFriends[k][0], sharedFriends[k].count() * 10))
+    for (k in sharedFriendList.indices) {
+        if (!sharedFriendsScore.toString().contains(sharedFriendList[k])) {
+            sharedFriendsScore.add(
+                listOf(
+                    sharedFriendList[k],
+                    sharedFriendList.count { it == sharedFriendList[k] } * 10))
         }
-        println(sharedFriendsScore.toString())
+    }
+    for (l in visitors.indices) {
+        for (m in sharedFriendsScore.indices) {
+            when {
+                !userFriendList.contains(visitors[l]) && !sharedFriendsScore.toString().contains(visitors[l]) -> {
+                    sharedFriendsScore.add(listOf(visitors[l], 1))
+                }
+
+                !userFriendList.contains(visitors[l]) && sharedFriendsScore[m][0] == visitors[l] -> {
+                    sharedFriendsScore[m] = listOf(visitors[l], sharedFriendsScore[m][1] as Int + 1)
+                }
+            }
+        }
     }
     return result
 }
